86c752762b021cd848dd7f319394c099d8d70192
mitchell.wilson <mitchell.wilson@epicgames.com>
2017-08-25 10:16:38 -0400

    Adding BP_FPSProjectile, sphere static mesh for projectile, moving static and skeletal mesh assets to correct location
    Step 3.2 - Implementing Shooting

--- Source/FPSProject/FPSCharacter.cpp
+++ Source/FPSProject/FPSCharacter.cpp
@@ -2,6 +2,7 @@
 
 #include "FPSCharacter.h"
 #include "Camera/CameraComponent.h"
+#include "FPSProjectile.h"
 
 
 // Sets default values
@@ -98,4 +99,33 @@ void AFPSCharacter::StopJump()
 
 void AFPSCharacter::Fire()
 {
+	// Attempt to fire a projectile.
+	if (ProjectileClass)
+	{
+		// Get the camera transform.
+		FVector CameraLocation;
+		FRotator CameraRotation;
+		GetActorEyesViewPoint(CameraLocation, CameraRotation);
+
+		// Transform MuzzleOffset from camera space to world space.
+		FVector MuzzleLocation = CameraLocation + FTransform(CameraRotation).TransformVector(MuzzleOffset);
+		FRotator MuzzleRotation = CameraRotation;
+		// Skew the aim to be slightly upwards.
+		MuzzleRotation.Pitch += 10.0f;
+		UWorld* World = GetWorld();
+		if (World)
+		{
+			FActorSpawnParameters SpawnParams;
+			SpawnParams.Owner = this;
+			SpawnParams.Instigator = Instigator;
+			// Spawn the projectile at the muzzle.
+			AFPSProjectile* Projectile = World->SpawnActor<AFPSProjectile>(ProjectileClass, MuzzleLocation, MuzzleRotation, SpawnParams);
+			if (Projectile)
+			{
+				// Set the projectile's initial trajectory.
+				FVector LaunchDirection = MuzzleRotation.Vector();
+				Projectile->FireInDirection(LaunchDirection);
+			}
+		}
+	}
 }
